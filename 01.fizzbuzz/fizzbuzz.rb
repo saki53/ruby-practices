83223c0338f6e54d61be999d4ce8aa553869fd65
saki53 <devyamasaki@gmail.com>
2025-09-13 17:46:36 +0900

    FizzBuzz課題

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz
+    for num in 1..20
+        if num % 15 == 0
+        puts "FizzBuzz"
+        elsif num % 3 == 0
+            puts "Fizz"
+        elsif num % 5 == 0
+            puts "Buzz"
+        else
+            puts num
+        end
+    end
+end
+fizzbuzz
